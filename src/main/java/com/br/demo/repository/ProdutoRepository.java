417b5dba4afbba965bd8c30c167d9d733c93b616
valentimdev <valentimdesenvolvedor@gmail.com>
2025-02-20 19:33:47 -0300

    atualizacoes

--- src/main/java/com/br/demo/repository/ProdutoRepository.java
+++ src/main/java/com/br/demo/repository/ProdutoRepository.java
@@ -18,4 +18,27 @@ public class ProdutoRepository {
     public Optional<Produto> findById(Long id) {
         return produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
     }
+    public Produto save(Produto produto){
+        produto.setId(nextId++);
+        produtos.add(produto);
+        return produto;
+    }
+    public void deleteById(Long id){
+        produtos.removeIf(p -> p.getId().equals(id));
+    }
+    public Produto update(Produto produto){
+        return produtos.stream()
+                .filter(p -> p.getId().equals(produto.getId()))
+                .findFirst()
+                .map(p->{
+                    p.setNome(produto.getNome());
+                    p.setPreco(produto.getPreco());
+                    p.setNumeroSerie(produto.getNumeroSerie());
+                    return p;
+                })
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado para atualizacão"));
+    }
+
+
+
 }
---
+++ src/main/java/com/br/demo/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.br.demo.service;
+
+import com.br.demo.dto.response.ProdutoResponseDTO;
+import com.br.demo.model.Produto;
+import com.br.demo.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+    public List<ProdutoResponseDTO> listarProdutos(){
+        return produtoRepository.findAll().stream()
+                .map(p -> new ProdutoResponseDTO(p.getId(), p.getNome(), p.getPreco(), p.getNumeroSerie()))
+                .collect(Collectors.toList());
+
+    }
+}
